1cdb74a643cb1e89e42cc77e6f58b804008455c3
巧椰 菊地 <220065@jc-21.jp>
2023-06-29 14:43:39 +0900

    aa

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ using std::cin;
 using std::cout;
 using std::endl;
 
-const int INF = 799797979;//まだ探索していない
+const int INF = 9999999;//まだ探索していない
 const int DONE = 1;//探索済み
 
 struct Edge
@@ -24,9 +24,9 @@ struct Edge
 int main(void) {
 
 	// { distance, from }
-	using Pair = std::pair<int, int>;
+	using Pair = std::pair<long long, long long>;
 
-	using Graph = vector<vector<int>>;
+	using Graph = vector<vector<long long>>;
 
 	Graph v;    //マップの情報を入れる
 	Graph dist; //マップの位置に連動してその頂点までどのぐらいの数で行けるか追加する
@@ -41,25 +41,28 @@ int main(void) {
 
 	for (int i = 0; i < h; i++) {
 		for (int f = 0; f < w; f++) {
-			cin >> v.at(i).at(f);
+			int n = 0;
+			cin >> n;
+			v.at(i).at(f) = n;
+			//dist.at(i).at(f) = n;
 		}
 	}
 
 	std::priority_queue<Pair, vector<Pair>, std::greater<Pair>> que; //探索済みの場所を記憶しておく。二次元配列じゃなくてPair型なのはわざわざ行き止まりの所を記録する必要はないため、一度行った場所だけを座標で横並びで覚えておけばいい
 	que.emplace(0, 0);//スタート地点から探索を始める（paizaは左上からだったため０、０）
-	dist.assign(h, vector<int>(w, INF));//初期化
+	dist.assign(h, vector<long long>(w, INF));//初期化
 	dist.at(0).at(0) = 0; //最初は既に探索済みのため
 	//dist.front() = { 0,0 };
-	
+
 	while (!que.empty())
 	{
 		Pair now = que.top();//今いる場所を確保
 		que.pop();
 
-		if (now.first == h - 1 && now.second == w - 1) { //ゴールなら
-			cout << dist.at(now.first).at(now.second);
-			break;
-		}
+		//if (now.first == h - 1 && now.second == w - 1) { //ゴールなら
+		//	cout << dist.at(now.first).at(now.second);
+		//	break;
+		//}
 
 		int dy[4] = { 0,0,1,-1 };//上下左右に移動（探索）するための配列。二つまとめて縦に見ると上下左右
 		int dx[4] = { 1,-1,0,0 };
@@ -74,17 +77,18 @@ int main(void) {
 			//if (v.at(ny).at(nx) == 1) {// 行き止まりなら
 			//	continue;
 			//}
-			if (dist.at(ny).at(nx) != INF) { //探索済みなら
+			//if (dist.at(ny).at(nx) != INF) { //探索済みなら
+			//	continue;
+			//}
+			if (dist.at(ny).at(nx) <= dist.at(now.first).at(now.second) + v.at(ny).at(nx)) { //これから探索するところが最短距離か
 				continue;
 			}
 			que.emplace(ny, nx);
 			dist.at(ny).at(nx) = dist.at(now.first).at(now.second) + v.at(ny).at(nx);
-
-
 		}
 	}
 
-	
+	cout << dist.at(h - 1).at(w - 1);
 
     return 0;
 }
